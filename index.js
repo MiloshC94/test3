7fa54bd51f34944b9d4a39d8a5f2b81bb3e2e79b
milosh@cybeats.com <milosh@cybeats.com>
2022-10-12 09:41:19 +0200

    as

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 console.log(process.env.info);
 var argv = require("minimist")(process.argv.slice(2));
-const url = argv.u.toString();
-console.log(url);
+// const url = argv.u.toString();
+console.log(argv);
